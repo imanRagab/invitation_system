9823003fa2d9803482a246df7d400026b28e3477
Iman <eman.ragab@objects.ws>
2023-11-09 01:56:30 +0100

    Modify entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
- 
+
 composer install -n
+
+bin/console doctrine:database:create --no-interaction
 bin/console doc:mig:mig --no-interaction
 bin/console doc:fix:load --no-interaction
- 
+
 exec "$@"
