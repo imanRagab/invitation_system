3979ab983a12d10ef8fa5853d51e75390415b074
Iman <eman.ragab@objects.ws>
2023-11-09 01:08:27 +0100

    Refactor InvitationController

--- src/Controller/API/V1/InvitationController.php
+++ src/Controller/API/V1/InvitationController.php
@@ -15,24 +15,40 @@ use App\Utils\InvitationConstants;
 
 class InvitationController extends AbstractController
 {
-    public function sendInvitation(
-        Request $request, EntityManagerInterface $entityManager, 
-        SerializerInterface $serializer): Response
+    /**
+     *
+     * @var EventDispatcherInterface
+     */
+    protected $EntityManagerInterface;
+    /**
+     *
+     * @var Registry
+     */
+    protected $workflowRegistry;
+
+    public function __construct(
+        EntityManagerInterface $entityManager,
+        Registry $workflowRegistry)    
+    {
+        $this->entityManager = $entityManager;
+        $this->workflowRegistry = $workflowRegistry;
+    }
+    public function sendInvitation(Request $request, SerializerInterface $serializer): Response
     {
         $invitation = new Invitation();
         
-        $senderReference = $entityManager->getReference(
+        $senderReference = $this->entityManager->getReference(
                                 User::class, $request->request->get('sender_id')
                         );
-        $invitedReference = $entityManager->getReference(
+        $invitedReference = $this->entityManager->getReference(
                                 User::class, $request->request->get('invited_id')
                         );
         
         $invitation->setSender($senderReference);
         $invitation->setInvited($invitedReference);
 
-        $entityManager->persist($invitation);
-        $entityManager->flush();
+        $this->entityManager->persist($invitation);
+        $this->entityManager->flush();
 
         $jsonContent = $serializer->serialize($invitation, 'json', [
             'groups' => 'invitation_create',
@@ -40,17 +56,17 @@ class InvitationController extends AbstractController
         return new Response($jsonContent, Response::HTTP_CREATED, ['Content-Type' => 'application/json']);
     }
 
-    public function acceptInvitation(Request $request, Invitation $invitation,
-                    Registry $workflowRegistry, EntityManagerInterface $entityManager): Response
+    public function acceptInvitation(Request $request, Invitation $invitation): Response
     {
-        $workflow = $workflowRegistry->get($invitation, 'invitation');
+        $workflow = $this->workflowRegistry->get($invitation, 'invitation');
 
         if ($workflow->can($invitation, 'accept')) {
             $workflow->apply($invitation, 'accept');
-            $entityManager->persist($invitation);
-            $entityManager->flush();
+            $this->entityManager->persist($invitation);
+            $this->entityManager->flush();
             return new Response(InvitationConstants::ACCEPT_SUCCESS_MSG, Response::HTTP_OK);
         }
         return new Response(InvitationConstants::ACCEPT_ERROR_MSG, Response::HTTP_BAD_REQUEST);
     }
+
 }
